aca49fb7e714e605ef505bb0db12691efcaea6be
Matthew Inman <Matthew.Inman@cdl.co.uk>
2015-12-23 12:56:07 +0000

    Made some changes to the RankTypes enum so we can get a Rank level so it makes it easier to determine what rank is higher

--- policeapi/src/main/java/uk/co/pped/policeapi/utilities/RankTypes.java
+++ policeapi/src/main/java/uk/co/pped/policeapi/utilities/RankTypes.java
@@ -1,42 +1,144 @@
 package uk.co.pped.policeapi.utilities;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 
 public enum RankTypes {
+
+	PCSO("PCSO") {
+		@Override
+		public int getRankGrade() {
+			return PCSO_INT;
+		}
+	},
 	
-	private static final Integer PCSO_INT = 1;
-	private static final Integer CONSTALBE_INT = 2;
-	
-	PCSO("PCSO"),
 	SC("SC") {
-		public final int getRankLevel() {
+		@Override
+		public int getRankGrade() {
 			return CONSTALBE_INT;
 		}
 	},
-	PC("PC"),
-	DC("DC"),
-	SS("SS"),
-	PS("PS"),
-	DS("DS"),
-	SI("SI"),
-	PI("PI"),
-	DI("DI"),
-	SCI("SCI"),
-	PCI("PCI"),
-	DCI("DCI"),
-	SCO("SCO"),
-	SP("SP"),
-	CSP("CSP"),
-	ACC("ACC"),
-	DCC("DCC"),
-	CC("CC"),
-	UNKNOWN("UU");
-	public abstract int getRankLevel();
+	PC("PC") {
+		@Override
+		public int getRankGrade() {
+			return CONSTALBE_INT;
+		}
+	},
+	DC("DC") {
+		@Override
+		public int getRankGrade() {
+			return CONSTALBE_INT;
+		}
+	},
+	SS("SS") {
+		@Override
+		public int getRankGrade() {
+			return SERGEANT_INT;
+		}
+	},
+	PS("PS") {
+		@Override
+		public int getRankGrade() {
+			return SERGEANT_INT;
+		}
+	},
+	DS("DS") {
+		@Override
+		public int getRankGrade() {
+			return SERGEANT_INT;
+		}
+	},
+	SI("SI") {
+		@Override
+		public int getRankGrade() {
+			return INSPECTOR_INT;
+		}
+	},
+	PI("PI") {
+		@Override
+		public int getRankGrade() {
+			return INSPECTOR_INT;
+		}
+	},
+	DI("DI") {
+		@Override
+		public int getRankGrade() {
+			return INSPECTOR_INT;
+		}
+	},
+	SCI("SCI") {
+		@Override
+		public int getRankGrade() {
+			return CHIEF_INSPECTOR_INT;
+		}
+	},
+	PCI("PCI") {
+		@Override
+		public int getRankGrade() {
+			return CHIEF_INSPECTOR_INT;
+		}
+	},
+	DCI("DCI") {
+		@Override
+		public int getRankGrade() {
+			return CHIEF_INSPECTOR_INT;
+		}
+	},
+	SCO("SCO") {
+		@Override
+		public int getRankGrade() {
+			return SPECIALS_CHIEF_OFFICER;
+		}
+	},
+	SP("SP") {
+		@Override
+		public int getRankGrade() {
+			return SUPERINTENDENT_INT;
+		}
+	},
+	CSP("CSP") {
+		@Override
+		public int getRankGrade() {
+			return CHIEF_SUPERINTENDENT_INT;
+		}
+	},
+	ACC("ACC") {
+		@Override
+		public int getRankGrade() {
+			return ACC_INT;
+		}
+	},
+	DCC("DCC") {
+		@Override
+		public int getRankGrade() {
+			return DCC_INT;
+		}
+	},
+	CC("CC") {
+		@Override
+		public int getRankGrade() {
+			return CC_INT;
+		}
+	},
+	UNKNOWN("UU") {
+		@Override
+		public int getRankGrade() {
+			return UNKNOWN_INT;
+		}
+	};
+	
+	public abstract int getRankGrade();
+	private static final Integer UNKNOWN_INT = 0;
 	private static final Integer PCSO_INT = 1;
 	private static final Integer CONSTALBE_INT = 2;
+	private static final Integer SERGEANT_INT = 3;
+	private static final Integer INSPECTOR_INT = 4;
+	private static final Integer CHIEF_INSPECTOR_INT = 5;
+	private static final Integer SPECIALS_CHIEF_OFFICER = 6;
+	private static final Integer SUPERINTENDENT_INT = 7;
+	private static final Integer CHIEF_SUPERINTENDENT_INT = 8;
+	private static final Integer ACC_INT = 9;
+	private static final Integer DCC_INT = 10;
+	private static final Integer CC_INT = 11;
 	
 	private final String rankType;
 	
@@ -148,8 +250,4 @@ public enum RankTypes {
 		}
 	}
 	
-	public static Map<RankTypes, Integer> ranks() {
-		return ranks;
-	}
-
 }
